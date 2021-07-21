218dbd19a5a115918b613a4e6eb5f9f965c33929
Edson0710 <edsonnavarro0710@gmail.com>
2021-07-21 13:34:29 -0500

    Directivas estructurales

--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -8,16 +8,31 @@ import { Zapatilla } from "../models/zapatilla";
 export class ZapatillasComponent implements OnInit{
     public titulo: string = "Componente de Zapatillas";
     public zapatillas:Array<Zapatilla>;
+    public marcas:String[];
+    public color:string;
     constructor(){
+        this.color = 'orange';
+        this.marcas = new Array;
         this.zapatillas = [
             new Zapatilla('Reebok Classic', 'Reebok', 'blanco', 80, true),
             new Zapatilla('jordan 1', 'Nike', 'negras', 60, true),
             new Zapatilla('Airforce', 'Nike', 'negras', 70, true),
-            new Zapatilla('forum', 'adidas', 'rosa', 180, false)
+            new Zapatilla('forum', 'Adidas', 'rosa', 180, false)
         ];
     }
 
     ngOnInit(){
         console.log(this.zapatillas);
+        this.getMarcas();
+    }
+
+    getMarcas(){
+        this.zapatillas.forEach((zapatilla, index) => {
+            //  Ingresar una marca si no está ya agregada
+            if(this.marcas.indexOf(zapatilla.marca) < 0){
+                this.marcas.push(zapatilla.marca);
+            }
+        });
+        console.log(this.marcas);
     }
 }
