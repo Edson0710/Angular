cbd57aaec4b45b449af2e36902f0b325874c5068
Edson0710 <edsonnavarro0710@gmail.com>
2021-07-28 12:32:01 -0500

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
 
-const routes: Routes = [];
+//Importar componentes
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+
+//  Array de rutas
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'zapatillas', component: ZapatillasComponent},
+  {path: 'videojuego', component:VideojuegoComponent},
+  {path: 'cursos', component: CursosComponent},
+  // {path: 'cursos/:nombre/:apellidos', component: CursosComponent},
+  {path: 'cursos/:nombre', component: CursosComponent},
+  {path: 'cursos/:nombre/:followers', component: CursosComponent},
+  // Importante
+  {path: '**', component: HomeComponent}
+];
+
+//  Exportar el modulo del router
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Params } from '@angular/router';
 
 @Component({
   selector: 'cursos',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
-
-  constructor() { }
+  public nombre: string = "";
+  public followers: number = 0;
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) { }
 
   ngOnInit(): void {
+    this._route.params.subscribe((params: Params) => {
+      this.nombre = params.nombre;
+      // this.nombre = params['nombre'];
+      // + para convertir de string a number
+      this.followers = +params.followers;
+      console.log(this.nombre);
+      // console.log(typeof +params.followers);
+      if(this.nombre == 'ninguno'){
+        this._router.navigate(['/home']);
+      }
+    });
+  }
+
+  redirigir(){
+    this._router.navigate(['/zapatillas']);
   }
 
 }
